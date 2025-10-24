efdda15d4531abea3de33f056979e0697aada8b7
RafaelMRos <143029342+RafaelMRos@users.noreply.github.com>
2025-10-24 20:16:20 -0300

    Update pratoModel.java

--- BuchoCheio/src/main/java/buchocheio/com/example/BuchoCheio/Model/pratoModel.java
+++ BuchoCheio/src/main/java/buchocheio/com/example/BuchoCheio/Model/pratoModel.java
@@ -10,5 +10,52 @@ public class pratoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private float preco;
+    private String nome;
+    private String descricao;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public pratoModel() {
+        this.preco=-1;
+        this.nome = "";
+        this.descricao = "";
+        
+    }
+    public pratoModel(Long id,float preco, String nome, String descricao) {
+        this.id = id;
+        this.preco = preco;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
 }
